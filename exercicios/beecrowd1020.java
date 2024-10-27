41830adc4d77370d7915651f5f76339957b4b919
Gabriel Chaves <chaves_br@hotmail.com>
2024-10-27 20:39:50 -0300

    Update beecrowd1020.java

--- exercicios/beecrowd1020.java
+++ exercicios/beecrowd1020.java
@@ -7,15 +7,12 @@ public class beecrowd1020 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        // Lê a idade em dias
         int dias = sc.nextInt();
 
-        // Calcula anos, meses e dias
         int ano = dias / 365;
         int mes = (dias % 365) / 30;
         int dia = (dias % 365) % 30;
 
-        // Imprime o resultado, garantindo que cada parte esteja em uma nova linha
         System.out.printf("%d ano(s)\n", ano);
         System.out.printf("%d mes(es)\n", mes);
         System.out.printf("%d dia(s)\n", dia);
