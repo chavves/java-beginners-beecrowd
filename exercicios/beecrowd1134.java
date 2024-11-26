6ddc3c32024b2ccc1840f9b5b50a061e9b3b20c6
Gabriel Chaves <chaves_br@hotmail.com>
2024-11-26 18:59:58 -0300

    while

---
+++ exercicios/beecrowd1134.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class beecrowd1134 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int numProduto;
+        int A = 0, D = 0, G = 0;
+        String abastecimento;
+
+        while (true) {
+            numProduto = sc.nextInt();
+
+
+            switch (numProduto) {
+                case 1:
+                    A += 1;
+                    break;
+                case 2:
+                    G += 1;
+                    break;
+                case 3:
+                    D += 1;
+                    break;
+                case 4:
+                    System.out.println("MUITO OBRIGADO");
+                    System.out.println("Alcool: " + A);
+                    System.out.println("Gasolina: " + G);
+                    System.out.println("Diesel: " + D);
+                    sc.close();
+                    return;
+                default:
+                    break;
+            }
+        }
+    }
+}
